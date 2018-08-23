3ac92665b4acb4a8626669421ddb0d05e18608a5
Chris Chen <chiandeye@gmail.com>
2018-08-23 11:43:06 +0800

    Add html, css, js

---
+++ all.js
@@ -0,0 +1,16 @@
+var app = new new Vue({
+  el: '#app',
+  data: {
+    newTodo: '',
+    todos: [{
+      id: '123',
+      title: 'HI',
+      completed: false
+    }],
+  },
+  methods: {
+    addTodo: function () {
+
+    }
+  }
+});
